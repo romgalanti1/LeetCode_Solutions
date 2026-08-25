299d28c63be62d76c02adf7f26d25d6b5140f204
romgalanti1 <romgalanti@mail.tau.ac.il>
2026-08-25 15:46:52 +0300

    Time: 9 ms (56.14%), Space: 12.3 MB (57.9%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,14 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        alphabet={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        res=0
-        for i in range(len(s)-1):
-            if s[i]=='I' or s[i]=='X' or s[i]=='C':
-                if alphabet[s[i]]<alphabet[s[i+1]]:
-                    res-=alphabet[s[i]]
-                    continue
-            res+=alphabet[s[i]]
-        res+=alphabet[s[-1]]
-        return res 
-        
+        my_map = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+
+        ans = my_map[s[0]]
+
+        for i in range(1,len(s)):
+            if(my_map[s[i]] > my_map[s[i-1]]):
+                ans += my_map[s[i]]
+                ans -= 2 * my_map[s[i-1]]
+            else:
+                ans += my_map[s[i]]        
+
+        return ans
