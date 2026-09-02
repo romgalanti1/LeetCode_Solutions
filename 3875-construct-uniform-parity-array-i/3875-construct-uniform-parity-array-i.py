348fca5d1cb391bb279eba3c965da6aeb6a61bd2
romgalanti1 <romgalanti@mail.tau.ac.il>
2026-09-02 18:03:45 +0300

    Time: 0 ms (100%), Space: 12.3 MB (96.25%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -4,7 +4,4 @@ class Solution(object):
         :type nums1: List[int]
         :rtype: bool
         """
-        odd_count=(1 for x in nums1 if x % 2 != 0)
-        can_be_even = odd_count != 1
-        can_be_odd = odd_count == len(nums1) or (odd_count>=1)
-        return can_be_even or can_be_odd        
+        return True
