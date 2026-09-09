265e2df4531c919db739f3864b41fdb001609955
romgalanti1 <romgalanti@mail.tau.ac.il>
2026-09-10 00:46:54 +0300

    Time: 0 ms (100%), Space: 12.5 MB (0%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -4,11 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        commas=1
         start=1000
         res=0
         while n>=start:
             res+= (n-start) + 1
             start=start * 1000
-            commas+=1
         return res
